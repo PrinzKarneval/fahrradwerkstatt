2f4c56de3889e9fecfa3d40f4e6da8c3433e32c6
Franz Schwerdtmann <franz.schwerdtmann@gmail.com>
2026-02-06 08:58:50 +0100

    Fixed path

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.expanduser('~/DjangoProject')
+path = os.path.expanduser('~/Fahrradwerkstatt')
 if path not in sys.path:
     sys.path.insert(0, path)
 
